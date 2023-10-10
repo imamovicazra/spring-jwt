57035668b9e1257ca59af71b5848b22ebce44a7a
Azra Imamovic <azra.imamovic998@gmail.com>
2023-10-10 14:51:10 +0200

    Implementation of jwt auth filter

--- spring-jwt/src/main/java/springjwt/service/JwtService.java
+++ spring-jwt/src/main/java/springjwt/service/JwtService.java
@@ -74,4 +74,18 @@ public class JwtService {
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
+
+    public boolean isTokenValid(String token, UserDetails userDetails)
+    {
+        final String username=extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return  extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
 }
